3ade300f83e7ad4658990d12002b476da9ccab6e
trallgorm <the_russian_vsn@hotmail.com>
2021-07-15 23:23:46 -0400

    Adding a backup API endpoint for balance checking due to errors frequently returned by the original

--- pywallet.py
+++ pywallet.py
@@ -122,6 +122,8 @@ passphrase = ""
 global_merging_message = ["",""]
 
 balance_site = 'https://blockchain.info/q/addressbalance/'
+backup_balance_site ='https://api.blockcypher.com/v1/btc/main/addrs/'
+
 aversions = {}
 for i in range(256):
 	aversions[i] = "version %d" % i;
@@ -2933,7 +2935,12 @@ def importprivkey(db, sec, label, reserve, verbose=True):
 
 def balance(site, address):
 	page=urllib.urlopen("%s%s" % (site, address))
-	return page.read()
+	query_result=page.read()
+	#If the initial API call returned an error, use a secondary API
+	if query_result.startswith("error"):
+		page = urllib.urlopen("%s%s" % (backup_balance_site, address))
+		query_result = json.loads(page.read())["balance"]
+	return query_result
 
 def read_jsonfile(filename):
 	filin = open(filename, 'r')
